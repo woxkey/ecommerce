ad8d1f13cefc77471129475479bbb733fc6c6a80
Alen Bolatov <alenbolatov@Alens-MacBook-Pro.local>
2024-02-23 22:17:51 -0500

    add related products component

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore ab nisi eligendi, impedit, voluptates esse placeat, repudiandae nobis doloremque veritatis earum provident? Eum unde aperiam vitae sequi totam, illo repudiandae repellat accusantium cupiditate ipsa excepturi culpa maiores porro provident alias iusto nihil sapiente delectus! Dolorem delectus blanditiis reprehenderit officiis!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore ab nisi eligendi, impedit, voluptates esse placeat, repudiandae nobis doloremque veritatis earum provident? Eum unde aperiam vitae sequi totam, illo repudiandae repellat accusantium cupiditate ipsa excepturi culpa maiores porro provident alias iusto nihil sapiente delectus! Dolorem delectus blanditiis reprehenderit officiis!</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
